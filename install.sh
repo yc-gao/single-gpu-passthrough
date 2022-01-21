d61d565d4e77e8a673169721992472316e5e04d2
xundaoxd <xundaoxd@outlook.com>
2022-01-21 23:05:12 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-if [ -d /etc/libvirt/hooks ]; then
+if [ -f /etc/libvirt/hooks/qemu -o -d /etc/libvirt/hooks/qemu.d ]; then
     echo 'Error: /etc/libvirt/hooks exists.'
     exit 1
 fi
-
 
 if [ -f /etc/systemd/system/libvirt-nosleep@.service ]; then
     echo 'Error: /etc/systemd/system/libvirt-nosleep@.service exists.'
@@ -15,7 +14,6 @@ if [ ! -d /usr/share/vgabios/ ]; then
     mkdir -p /usr/share/vgabios/
 fi
 
-cp -r libvirt-hooks /etc/libvirt/hooks
+cp -r libvirt-hooks/* /etc/libvirt/hooks/
 cp libvirt-nosleep@.service /etc/systemd/system/libvirt-nosleep@.service 
 cp rom/* /usr/share/vgabios/
-
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rm -rf /etc/libvirt/hooks
+rm -rf /etc/libvirt/hooks/qemu
+rm -rf /etc/libvirt/hooks/qemu.d
 rm -rf /etc/systemd/system/libvirt-nosleep@.service
 rm -rf /usr/share/vgabios/
