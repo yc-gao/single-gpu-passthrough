70934f11ba003d1041230f5cc55daa8c7f7e1f0d
yinchaogao <yinchaogao@deeproute.ai>
2022-04-18 19:06:43 +0800

    use soft link instead of copying

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+script_path=$(realpath $0)
+script_dir=$(dirname $script_path)
+
 if [ -f /etc/libvirt/hooks/qemu -o -d /etc/libvirt/hooks/qemu.d ]; then
     echo 'Error: /etc/libvirt/hooks exists.'
     exit 1
@@ -14,6 +17,7 @@ if [ ! -d /usr/share/vgabios/ ]; then
     mkdir -p /usr/share/vgabios/
 fi
 
-cp -r libvirt-hooks/* /etc/libvirt/hooks/
-cp libvirt-nosleep@.service /etc/systemd/system/libvirt-nosleep@.service 
-cp rom/* /usr/share/vgabios/
+ln -s $script_dir/libvirt-hooks/qemu /etc/libvirt/hooks/qemu
+ln -s $script_dir/libvirt-hooks/qemu.d /etc/libvirt/hooks/qemu.d
+ln -s $script_dir/libvirt-nosleep@.service /etc/systemd/system/libvirt-nosleep@.service 
+ln -s $script_dir/rom/Gigabyte.RTX3090.24576.210315_3.rom /usr/share/vgabios/Gigabyte.RTX3090.24576.210315_3.rom
